8443689dc21dd1fd79005b846d86d7ad0b2dda59
saadakram-3212 <saad.akram@eurustechnologies.com>
2026-02-04 00:03:04 +0500

    removing unnecessary variables

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 
 module "bedrock" {
-  source                = "./modules/knowledgebase" # local example
+  source                = "./modules/knowledgebase" 
   create_default_kb     = true
-  create_agent          = false
   create_s3_data_source = true
 }
